98f15b231f330a60e48592cdc4e30ddfb024e733
Hector Mercadal <46133563+hmercadal@users.noreply.github.com>
2019-06-05 01:12:01 -0600

    escritorio

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -13,20 +13,20 @@ require 'header.php';
 
 if ($_SESSION['escritorio']==1)
 {
-  // require_once "../modelos/consultas_compromisos.php";
-  //
-  // $consultas_compromisos = new Consultas_compromisos();
-  // $rsptac = $consultas_compromisos->totalcompromisoshoy();
-  // $regc=$rsptac->fetch_object();
-  // $totalc=$regc->total_compra;
-  //
-  // $rsptav = $consultas_compromisos->totalctasbancariashoy();
-  // $regv=$rsptav->fetch_object();
-  // $totalv=$regv->fondos_disponibles;
-  //
-  // $rsptab = $consultas_compromisos->totaladministrar_ordeneshoy();
-  // $regb=$rsptab->fetch_object();
-  // $totalb=$regb->total_neto;
+  require_once "../modelos/consultas_compromisos.php";
+
+  $consultas_compromisos = new Consultas_compromisos();
+  $rsptac = $consultas_compromisos->totalcompromisoshoy();
+  $regc=$rsptac->fetch_object();
+  $totalc=$regc->total_compra;
+
+  $rsptav = $consultas_compromisos->totalctasbancariashoy();
+  $regv=$rsptav->fetch_object();
+  $totalv=$regv->fondos_disponibles;
+
+  $rsptab = $consultas_compromisos->totaladministrar_ordeneshoy();
+  $regb=$rsptab->fetch_object();
+  $totalb=$regb->total_neto;
 
 ?>
 <!--Contenido-->
