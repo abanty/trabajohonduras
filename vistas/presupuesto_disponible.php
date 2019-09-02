d30cf07b026dff5745b05bc80774698a2d3f55e6
abanty <abantybredda@gmail.com>
2019-09-02 00:46:27 -0500

    ss

--- vistas/presupuesto_disponible.php
+++ vistas/presupuesto_disponible.php
@@ -13,18 +13,13 @@ if ($_SESSION['siafi']==1)
 {
 ?>
 <style media="screen">
-
 form#formulario .help-block {
     margin-bottom: -5px !important;
     color: #dd4b39 !important;
     font-size: 12px !important;
 }
-
 </style>
-<!--Contenido-->
-      <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
-        <!-- Main content -->
         <section class="content-header">
           <h1>
             Administrar Presupuesto Disponible
@@ -44,8 +39,6 @@ form#formulario .help-block {
                         <div class="box-tools pull-right">
                         </div>
                     </div>
-                    <!-- /.box-header -->
-                    <!-- centro -->
                     <div class="panel-body table-responsive" id="listadoregistros">
                         <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover" width="100%">
                           <thead style="background-color:#d2d6de">
@@ -71,46 +64,39 @@ form#formulario .help-block {
                                   <input type="text" class="form-control input-sm" name="nombre_objeto" id="nombre_objeto" maxlength="100"  placeholder="Nombre de Objeto">
                                   <div class="messages"></div>
                                 </div>
-
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="grupo">Grupo:(*)</label>
                                   <input type="text" class="form-control input-sm"  name="grupo" id="grupo" maxlength="45" placeholder="Grupo">
                                   <div class="messages"></div>
                                 </div>
-
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="subgrupo">Sub-Grupo:(*)</label>
                                   <input type="text" class="form-control input-sm"  name="subgrupo" id="subgrupo" maxlength="45" placeholder="Sub Grupo">
                                   <div class="messages"></div>
                                 </div>
                               </div>
-
                               <div class="row">
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="codigo">Codigo:(*)</label>
                                   <input type="text" class="form-control input-sm" name="codigo" id="codigo" maxlength="45" placeholder="codigo">
                                   <div class="messages"></div>
                                 </div>
-
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="pres_aprobado">Pres. Inicial:</label>
                                   <input type="text" class="form-control input-sm" onchange="sumarcampos()" onkeyup="sumarcampos()" name="pres_aprobado" id="pres_aprobado" required>
                                   <div class="messages"></div>
                                 </div>
-
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="pres_modificado">Disminuciones:</label>
                                   <input type="text" class="form-control input-sm"  onchange="sumarcampos()" onkeyup="sumarcampos()" name="pres_modificado" id="pres_modificado" required>
                                   <div class="messages"></div>
                                 </div>
-
                                 <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                   <label for="presupuesto_anual">Por Ejecutar:</label>
                                   <input type="text" class="form-control input-sm"  name="presupuesto_anual" id="presupuesto_anual" required>
                                   <div class="messages"></div>
                                 </div>
                               </div>
-
                               <div class="row">
                                   <div class="form-group col-lg-3 col-md-3 col-sm-3 col-xs-12">
                                     <label for="fondos_disponibles">Pres. SIAFI Ejecutado:</label>
@@ -119,23 +105,17 @@ form#formulario .help-block {
                                   </div>
                                </div>
                             </div>
-                            <!-- /.box-body -->
-
                             <div class="box-footer">
                               <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
                               <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                             </div>
                           </form>
-
-              <!-- </form> -->
                     </div>
-                    <!--Fin centro -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-          </div><!-- /.row -->
-      </section><!-- /.content -->
-    </div><!-- /.content-wrapper -->
-  <!--Fin-Contenido-->
+                  </div>
+              </div>
+          </div>
+      </section>
+    </div>
 <?php
 }
 else
@@ -144,9 +124,7 @@ else
 }
 require 'footer.php';
 ?>
-
 <script type="text/javascript" src="scripts/presupuesto_disponible.js"></script>
-
 <?php
 }
 ob_end_flush();
